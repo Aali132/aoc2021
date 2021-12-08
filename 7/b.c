d4255cb6542950d6ad1ec60c91e27f4b74a03049
Aali132 <github@aali.email>
2021-12-08 13:48:42 +0100

    Advent of code day 7

---
+++ 7/b.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+char line[1024 * 1024];
+int crabs[1024];
+int num_crabs;
+int max_position;
+int best_fuel = INT_MAX;
+
+int aoc(FILE *input) {
+  fgets(line, sizeof(line), input);
+
+  char *position = strtok(line, ",");
+
+  while(position) {
+    int crab = atoi(position);
+    
+    if(crab > max_position) {
+      max_position = crab;
+    }
+    
+    crabs[num_crabs++] = crab;
+
+    position = strtok(0, ",");
+  }
+
+  for(int p = 0; p <= max_position; p++) {
+    int total_fuel = 0;
+    
+    for(int c = 0; c < num_crabs; c++) {
+      int distance = abs(p - crabs[c]);
+      
+      total_fuel += distance * (distance + 1) / 2;
+    }
+    
+    if(total_fuel < best_fuel) {
+      best_fuel = total_fuel;
+    }
+  }
+
+  printf("%i\n", best_fuel);
+
+  return 0;
+}
